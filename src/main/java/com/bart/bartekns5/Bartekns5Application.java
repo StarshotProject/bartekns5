1984b04c4d244f4351089ef0d0a138be1b064611
StarshotProject <bartoszsamolyk2@gmail.com>
2025-02-16 18:49:41 +0100

    initial commit

---
+++ src/main/java/com/bart/bartekns5/Bartekns5Application.java
@@ -0,0 +1,13 @@
+package com.bart.bartekns5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bartekns5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bartekns5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bart/bartekns5/Bartekns5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bart.bartekns5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bartekns5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
